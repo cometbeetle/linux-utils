e8f0f529e1a679d0f744589b21ffc0cb3ba96480
Ethan Mentzer <26334891+cometbeetle@users.noreply.github.com>
2025-10-21 13:20:39 -0400

    some uploads

---
+++ containers/dev/install-custom.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Add the Adoptium repository.
+cat <<EOF > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+EOF
+
+# Add the Visual Studio Code repository.
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+# Install the new packages.
+sudo dnf check-update
+sudo dnf install -y temurin-21-jdk code
+
