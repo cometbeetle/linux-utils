938fe3d904c992a7b44359b3c9d90f1585cfccfc
Ethan Mentzer <26334891+cometbeetle@users.noreply.github.com>
2025-10-21 15:26:25 -0400

    it works

--- containers/dev/install-custom.sh
+++ containers/dev/install-custom.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use setup directory from Dockerfile.
+cd /tmp/setup
+
 # Add the Adoptium repository.
 cat <<EOF > /etc/yum.repos.d/adoptium.repo
 [Adoptium]
@@ -18,3 +21,39 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 sudo dnf check-update
 sudo dnf install -y temurin-21-jdk code
 
+# Install JetBrains IDEs.
+PYCHARM_VERSION="2025.2.3"
+INTELLIJ_VERSION="2025.2.3"
+RUSTROVER_VERSION="2025.2.3"
+CLION_VERSION="2025.2.3"
+wget "https://download.jetbrains.com/python/pycharm-${PYCHARM_VERSION}.tar.gz"
+wget "https://download.jetbrains.com/idea/ideaIU-${INTELLIJ_VERSION}.tar.gz"
+wget "https://download.jetbrains.com/rustrover/RustRover-${RUSTROVER_VERSION}.tar.gz"
+wget "https://download.jetbrains.com/cpp/CLion-${CLION_VERSION}.tar.gz"
+sudo tar -xzvf pycharm-*.tar.gz -C /opt
+sudo tar -xzvf ideaIU-*.tar.gz -C /opt
+sudo tar -xzvf RustRover-*.tar.gz -C /opt
+sudo tar -xzvf CLion-*.tar.gz -C /opt
+rm *.tar.gz
+sudo mkdir /opt/jb_launchers
+cat <<EOF | sudo tee /opt/jb_launchers/pycharm.sh > /dev/null
+#!/bin/bash
+/opt/pycharm-*/bin/pycharm >/dev/null 2>&1 &
+EOF
+cat <<EOF | sudo tee /opt/jb_launchers/intellij.sh > /dev/null
+#!/bin/bash
+/opt/idea-IU-*/bin/idea >/dev/null 2>&1 &
+EOF
+cat <<EOF | sudo tee /opt/jb_launchers/rustrover.sh > /dev/null
+#!/bin/bash
+/opt/RustRover-*/bin/rustrover >/dev/null 2>&1 &
+EOF
+cat <<EOF | sudo tee /opt/jb_launchers/clion.sh > /dev/null
+#!/bin/bash
+/opt/clion-*/bin/clion >/dev/null 2>&1 &
+EOF
+sudo chmod +x /opt/jb_launchers/*.sh
+sudo ln -sf /opt/jb_launchers/pycharm.sh /usr/local/bin/pycharm
+sudo ln -sf /opt/jb_launchers/intellij.sh /usr/local/bin/intellij
+sudo ln -sf /opt/jb_launchers/rustrover.sh /usr/local/bin/rustrover
+sudo ln -sf /opt/jb_launchers/clion.sh /usr/local/bin/clion
