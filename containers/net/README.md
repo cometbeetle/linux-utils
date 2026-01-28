f2be44d8a307cb76b5ec833fc376bd805fa0e445
Ethan Mentzer <26334891+cometbeetle@users.noreply.github.com>
2026-01-28 17:42:24 -0500

    root box

---
+++ containers/net/README.md
@@ -0,0 +1,7 @@
+# Root Tools Toolbox
+
+## Important Notes
+
+- Run `distrobox assemble create` from within **this directory**, otherwise the
+  setup commands will fail.
+- This is a rooted container, which may make your system more vulnerable to attacks.
---
+++ containers/net/init.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# NOTE: This script assumes the distrobox instance will supply
+# the HOST_UID, HOST_GID, and HOST_HOME environment variables.
+# Ensure these are properly inserted by the host system.
+
+set -e
+
+# =======================================================================
+
+SENTINEL="$HOME/.distrobox-initialized"
+
+# Do not execute the script if the container has already been initialized.
+if [[ -f "$SENTINEL" ]]; then
+    exit 0
+fi
+touch "$SENTINEL"
+
+# =======================================================================
+
+# Use setup directory.
+cd /tmp/setup
+
+# Install standard packages via DNF.
+dnf install -y \
+    wireshark
+
+# Allow launching apps on the host system from within the container.
+ln -sf /usr/bin/distrobox-host-exec /usr/bin/flatpak
+ln -sf /usr/bin/distrobox-host-exec /usr/bin/xdg-open
+
+# Clean DNF.
+dnf clean all
+
+# Ensure proper container home directory permissions (must be done last).
+chown -R $HOST_UID:$HOST_GID $HOME
